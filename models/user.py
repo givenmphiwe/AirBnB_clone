b1cfa6dcf1bd3a2e3fc629e2cfde463253ab0a37
Given Makhubela <giggslive52@gmail.com>
2023-02-12 23:19:02 +0200

    bugs

--- models/user.py
+++ models/user.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
+"""user class, subclass of BaseModel
 """
-Defines the User class.
-"""
+
 from models.base_model import BaseModel
+import json
 
 
 class User(BaseModel):
-        """Represent a User
-        Attributes:
-            email (str): user email
-            password (str): user password
-            first_name (str): first name
-            last_name (str): last name
-        """
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    '''subclass of BaseModel class'''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
